60ddc8471f1877e5726b5261cc2dda600d463b22
carlosaguilardark22 <carlosaguilardark22@gmail.com>
2025-08-30 20:44:56 +0000

    do 1

--- astro-store/src/pages/api/auth/logout.ts
+++
@@ -1,6 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const get: APIRoute = async (context) => {
-  context.cookies.delete('session', { path: '/' });
-  return context.redirect('/auth/login');
-};
